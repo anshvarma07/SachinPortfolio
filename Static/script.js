d4bf94114727456f7138c6caddb893560991e0f3
Ansh Varma <96176123+anshvarma07@users.noreply.github.com>
2023-09-05 23:31:04 +0530

    First Commit

---
+++ Static/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const navUl = document.querySelector(".logo-ul");
+    const two = document.querySelector(".lets-connect");
+    const three = document.querySelector(".head-ul");
+    const proj = document.querySelector(".ProjHover");
+  
+    hamburgerMenu.addEventListener("click", function () {
+      navUl.classList.toggle("nav-open");
+      two.classList.toggle("nav-open");
+      three.classList.toggle("nav-open");
+    });
+  });
+
+
+  const navbar = document.querySelector('.nav');
+
+  // Function to add or remove the "scrolled" class based on scroll position
+  function toggleNavbarBackground() {
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  }
+
+  window.addEventListener('scroll', toggleNavbarBackground);
